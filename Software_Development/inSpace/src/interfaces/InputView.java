d34fcc8a4580ad7e7d954959348204fac28e3008
S Lynnette Wheeler <lynnettewheeler21@gmail.com>
2025-11-27 23:28:12 -0500

    Delete Software_Development/inSpace directory

--- Software_Development/inSpace/src/interfaces/InputView.java
+++
@@ -1,9 +0,0 @@
-package interfaces;
-
-/**
- * @author Logan
- * @lastModified 11/4/2020
- *
- * @description A tagging interface with no functionality, just used to group the various view input types
- */
-public interface InputView {}
